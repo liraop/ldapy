670d7b396a5f7b8ff570f54a3f1559c27fbbff04
liraop <lirapdo@gmail.com>
2018-02-09 15:04:29 -0300

    few routes to start the job and get to know Flask.

--- app/ldapy.py
+++ app/ldapy.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
-from ldap3 import Server, Connection
+from ldap3 import Server, Connection, ALL, NTLM
+from models.ldap_objects import Domain
+from utils import test_connection
 
 import os
 
@@ -9,27 +11,34 @@ app = Flask(__name__)
 registered_domains = []
 
 @app.route('/')
-def home():
-    return render_template('login.html')
+def index():
+    return render_template('index.html')
 
+@app.route('/login', methods=['POST'])
+def do_login():
+
+    return index()
 
 @app.route('/domain', methods=['POST'])
 def connection():
-    domain = request.form['domain']
+    domain = Domain(request.form['domain'], request.form ['user'], request.form['pass'], request.form['domainType'])
+    server = Server(domain.url, get_info=ALL)
 
-    server = Server(domain, get_info='ALL')
-    con = Connection(server, auto_bind=True)
+    if (domain.kind == 'AD'):
+        con = Connection(server, domain.url+"\\"+domain.bind_user , domain.bind_pw, authentication=NTLM)
+    elif (domain.kind == 'LDAP'):
+        con = Connection(server, domain.bind_user, domain.bind_pw, auto_bind=True)
 
     if (request.form['submit'] == 'Test Connection'):
-        if (con.bind()):
-            print("Connection successful")
+        if (test_connection(con)):
+            flash('Connection Successful!')
         else:
-            print("Connection failed")
+            flash('Connection Failed!')
     elif (request.form['submit'] == 'Register Domain'):
-        registered_domains.append(con)
-        print('added')
+        registered_domains.append(domain)
+        flash('Domain registered!')
 
-    return home()
+    return render_template('index.html', domain_list=registered_domains)
 
 
 if __name__ == '__main__':
